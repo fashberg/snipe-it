aaccdf9ecf1c422c0a9a67ff31778d42f8ffec0f
Folke Ashberg <folke.ashber@qplix.com>
2021-10-05 19:25:32 +0200

    Fix QR-Code Generation

--- app/Http/Middleware/PreventBackHistory.php
+++ app/Http/Middleware/PreventBackHistory.php
@@ -16,8 +16,12 @@ class PreventBackHistory
     public function handle($request, Closure $next)
     {
         $response = $next($request);
-        return $response->header('Cache-Control','no-cache, no-store, max-age=0, must-revalidate')
-            ->header('Pragma','no-cache')
-            ->header('Expires','Sun, 02 Jan 1990 00:00:00 GMT');
+	if (isset($response->header)){
+	    return $response->header('Cache-Control','no-cache, no-store, max-age=0, must-revalidate')
+		->header('Pragma','no-cache')
+		->header('Expires','Sun, 02 Jan 1990 00:00:00 GMT');
+	} else {
+	    return $response;
+	}
     }
 }
